dd473c3e9826fcc18ba9cc20b4623597a7f81115
cameronclass <kamrankutumov@gmail.com>
2022-05-17 22:16:04 +0300

    removed bootstrap, update swiper

--- js/main.js
+++ js/main.js
@@ -1,7 +1,1 @@
-// Preloader
-$( function() {
-    var $preloader = $('#page-preloader'),
-        $spinner   = $preloader.find('.spinner');
-    $spinner.fadeOut();
-    $preloader.delay(350).fadeOut('slow');
-});
+
